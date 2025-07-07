102bb868b67126514235af793fce8bcad5c68cc6
srishtisingh9866 <srishtisingh9866@gmail.com>
2025-07-07 14:09:06 +0530

    c14

---
+++ index10.js
@@ -0,0 +1,6 @@
+//arguments in node js
+//passing the argument from console
+const stName= process.argv[2] || 'Jhon'; //default argumnet would be jhon
+const stName2= process.argv[3] || 'Doe'; //default argumnet would be doe ;
+console.log(`Hello ${stName} ${stName2}`);  
+//code to run node server in same way
